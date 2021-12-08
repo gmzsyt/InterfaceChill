47213a3428428a5fdbdea9139942ffdbe6a1114f
Gamze Seyit <gmzzsyyt@gmail.com>
2021-12-09 01:18:30 +0300

    Add project files.

---
+++ InterfaceChill/IKrediManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceChill
+{
+    interface IKrediManager
+    {
+       void Hesapla();
+        void BiseyYap();
+
+    }
+}
---
+++ InterfaceChill/Program.cs
@@ -0,0 +1,22 @@
+﻿   using System;
+
+namespace InterfaceChill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TasitKrediManager kredi1 = new TasitKrediManager();
+            kredi1.Hesapla();
+
+            KonutKrediManager konurkredimanager = new KonutKrediManager();
+
+            IKrediManager kredi2 = new IhtiyacKrediManager();
+            kredi2.Hesapla();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konurkredimanager);
+
+        }
+    }
+}
